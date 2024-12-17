61ebdad57b63180771c0c82703333d121d75bf8d
zeafy <148182572+rlyrvra@users.noreply.github.com>
2024-12-17 13:04:03 +0800

    Hosting settings pt4

--- database/database.php
+++ database/database.php
@@ -16,7 +16,7 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ];
-
+$pdo;
 try {
     if(!$hosted){
         $pdo = new PDO(
