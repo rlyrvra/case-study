d8e8509d19f268402041982b1d3471d8516f613f
zeafy <148182572+rlyrvra@users.noreply.github.com>
2024-12-17 12:29:25 +0800

    Hosting settings

--- database/database.php
+++ database/database.php
@@ -2,6 +2,8 @@
 
 date_default_timezone_set('Asia/Manila');
 
+$hosted = false;
+
 $dataSourceName = 'mysql:host=localhost;port=3306;dbname=smart_wage;charset=utf8mb4';
 $dataSourceNameHosted = 'mysql:host=localhost;port=3306;dbname=u227551606_smartwage_db;charset=utf8mb4';
 
@@ -16,19 +18,24 @@ $options = [
 ];
 
 try {
-    $pdo = new PDO(
-        $dataSourceName,
-        $username,
-        $password,
-        $options
-    );
+    if(!$hosted){
+        $pdo = new PDO(
+            $dataSourceName,
+            $username,
+            $password,
+            $options
+        );
+    }
+    
 
-    // $pdo = new PDO(
-    //     $dataSourceNameHosted,
-    //     $usernameHosted,
-    //     $passwordHosted,
-    //     $options
-    // );
+    if ($hosted){
+        $pdo = new PDO(
+            $dataSourceNameHosted,
+            $usernameHosted,
+            $passwordHosted,
+            $options
+        );
+    }
 
 } catch (PDOException $exception) {
     error_log('Database Connection Error: Unable to connect to the database. ' .
