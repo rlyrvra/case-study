fac684f5ba623b79b9d4123e2b7ae6ad110815f2
zeafy <148182572+rlyrvra@users.noreply.github.com>
2025-02-06 08:06:57 +0800

    Revert "Removed default values from the `Holiday` model"
    This reverts commit 58ea07dc6cf525755c52692bb1082b3cf3ad946d.

--- database/database.php
+++ database/database.php
@@ -2,23 +2,40 @@
 
 date_default_timezone_set('Asia/Manila');
 
-$dataSourceName = 'mysql:host=localhost;port=3306;dbname=payroll;charset=utf8mb4';
+$hosted = ($_SERVER['HTTP_HOST'] !== 'localhost' && $_SERVER['HTTP_HOST'] !== '127.0.0.1');
+
+$dataSourceName = 'mysql:host=localhost;port=3306;dbname=smart_wage;charset=utf8mb4';
+$dataSourceNameHosted = 'mysql:host=localhost;port=3306;dbname=u227551606_smartwage_db;charset=utf8mb4';
 
 $username = 'root';
 $password = ''    ;
+$usernameHosted = 'u227551606_smartwage_user';
+$passwordHosted = 'smartWageaA@1'    ;
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ];
-
+$pdo;
 try {
-    $pdo = new PDO(
-        $dataSourceName,
-        $username      ,
-        $password      ,
-        $options
-    );
+    if(!$hosted){
+        $pdo = new PDO(
+            $dataSourceName,
+            $username,
+            $password,
+            $options
+        );
+    }
+    
+
+    if ($hosted){
+        $pdo = new PDO(
+            $dataSourceNameHosted,
+            $usernameHosted,
+            $passwordHosted,
+            $options
+        );
+    }
 
 } catch (PDOException $exception) {
     error_log('Database Connection Error: Unable to connect to the database. ' .
