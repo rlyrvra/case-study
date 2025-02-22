c73079e8b206ba9aea3bd181afd9f77fbe3f751d
Shiro-Hr <bulado.seanjocel.bscs2022@gmail.com>
2025-02-22 14:50:00 +0800

    Update work-schedule.php

--- work-schedule.php
+++ work-schedule.php
@@ -121,10 +121,9 @@ if($_SESSION['access_role'] !== 'Admin' && $_SESSION['access_role'] !== 'Manager
               <button type="button" class="btn btn-success btn-xl" data-bs-toggle="modal" data-bs-target="#add_work_schedules" onclick="fetchBreakTypes()">
                 <i class="bx bx-plus bx-lg"></i>Add Work Schedule
               </button>
-              
             </div>
-            <hr>
-            <div class="container-fluid mb-3 d-flex justify-content-between flex-column flex-lg-row">
+
+            <div class="container-fluid pt-5 mb-3 d-flex justify-content-between flex-column flex-lg-row">
               <h1 class="display-2">Breaks</h1>
               <button type="button" class="btn btn-success btn-xl" data-bs-toggle="modal" data-bs-target="#add_breaks">
                 <i class="bx bx-plus bx-lg"></i>Add Break Types
