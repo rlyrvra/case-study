647dcb4348c12edb24600113738d3ac9d19968b3
Shiro-Hr <bulado.seanjocel.bscs2022@gmail.com>
2025-02-22 14:36:30 +0800

    Update department-modals-update-form.php

--- departments/modules/department-modals-update-form.php
+++ departments/modules/department-modals-update-form.php
@@ -20,19 +20,19 @@
                 </div>
                 <form onsubmit="event.preventDefault();" id="update_departments_form">
                     <div class="mb-3">
-                        <label for="update_department_name" class="form-label">Department Name*</label>
+                        <label for="update_department_name" class="form-label">Department Name*:</label>
                         <input type="text" class="form-control" id="update_department_name" name="update_department_name" required value="">
                     </div>
                     <div class="mb-3">
-                        <label for="update_department_head" class="form-label">Department Head</label>
+                        <label for="update_department_head" class="form-label">Department Head:</label>
                         <select class="form-select" id="update_department_head" name="update_department_head" placeholder="Enter Department"></select>
                     </div>
                     <div class="mb-3">
-                        <label for="update_department_description" class="form-label">Department Description</label>
+                        <label for="update_department_description" class="form-label">Department Description:</label>
                         <textarea class="form-control" id="update_department_description" name="update_department_description"></textarea>
                     </div>
                     <div class="mb-3">
-                        <label for="update_department_status" class="form-label">Department Status*</label>
+                        <label for="update_department_status" class="form-label">Department Status*:</label>
                         <select class="form-select" id="update_department_status" name="update_department_status" value="">
                             <option value="Active">Active</option>
                             <option value="Inactive">Inactive</option>
