514cb5f1c9fa2f28efa7c44f58e15db99b2cf499
zeafy <148182572+rlyrvra@users.noreply.github.com>
2025-03-14 02:37:26 +0800

    Update generatePayslip.php

--- cron-jobs/generatePayslip.php
+++ cron-jobs/generatePayslip.php
@@ -96,8 +96,7 @@ $payrollGroupDao        = new PayrollGroupDao       ($pdo                   );
 $payrollGroupRepository = new PayrollGroupRepository($payrollGroupDao       );
 $payrollGroupService    = new PayrollGroupService   ($payrollGroupRepository);
 
-$originalCurrentDateTime = new DateTime('2025-01-07 00:00:00');
-echo $originalCurrentDateTime->format('l Y-m-d H:i:s A');
+$originalCurrentDateTime = new DateTime();
 
 $currentDateTime   =  clone $originalCurrentDateTime                             ;
 $currentDateTime   = (clone $currentDateTime)->modify('-1 day')                  ;
