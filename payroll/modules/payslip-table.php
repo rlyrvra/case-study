41325e761811afa00c4e803ed5ae7c5a3e20eaad
zeafy <148182572+rlyrvra@users.noreply.github.com>
2025-03-14 22:55:37 +0800

    Update payslip-table.php

--- payroll/modules/payslip-table.php
+++ payroll/modules/payslip-table.php
@@ -6,8 +6,6 @@
 </style>
 <!-- Table Rendering -->
 
-
-
 <table class="table table-striped table-hover table-bordered align-middle">
     <thead class="table-dark text-center align-middle">
       <tr>
@@ -84,19 +82,19 @@
     <ul class="pagination pagination-lg">
       <!-- Previous Button -->
       <li class="page-item <?= $page <= 1 ? 'disabled' : '' ?>">
-        <a class="page-link" onclick="fetchAllMyAttendance('prev')" aria-label="Previous">
+        <a class="page-link" onclick="fetchAllPayslips('prev')" aria-label="Previous">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <?php for ($i = 1; $i <= $totalPages; $i++): ?>
         <!-- Page Numbers -->
         <li class="page-item <?= $i === $page ? 'active' : '' ?>">
-          <a class="page-link" onclick="fetchAllMyAttendance(<?php echo $i ?>)" ><?= $i ?></a>
+          <a class="page-link" onclick="fetchAllPayslips(<?php echo $i ?>)" ><?= $i ?></a>
         </li>
       <?php endfor; ?>
       <!-- Next Button -->
       <li class="page-item <?= $page >= $totalPages ? 'disabled' : '' ?>">
-        <a class="page-link" onclick="fetchAllMyAttendance('next')" aria-label="Next">
+        <a class="page-link" onclick="fetchAllPayslips('next')" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
