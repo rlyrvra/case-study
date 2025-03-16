60c3b676f8af0f9cddbdb9da1329ce84f6bf066d
zeafy <148182572+rlyrvra@users.noreply.github.com>
2025-03-16 12:59:31 +0800

    Update overtime-rate.php

--- overtime-rate.php
+++ overtime-rate.php
@@ -166,9 +166,6 @@ $(document).ready(function() {
           (item.full_name
               ? '<span class="name">' + escape(item.full_name) + "</span>"
               : "") +
-          (item.description
-              ? '<span class="description">' + escape(item.email_address) + "</span>"
-              : "") +
           "</div>"
           );
       },
@@ -246,9 +243,6 @@ $(document).ready(function () {
           (item.title
               ? '<span class="name">' + escape(item.title) + "</span>"
               : "") +
-          (item.description
-              ? '<span class="description">' + escape(item.description) + "</span>"
-              : "") +
           "</div>"
           );
       },
@@ -320,9 +314,6 @@ $(document).ready(function () {
           "<div>" +
           (item.name
               ? '<span class="name">' + escape(item.name) + "</span>"
-              : "") +
-          (item.description
-              ? '<span class="description">' + escape(item.description) + "</span>"
               : "") +
           "</div>"
           );
